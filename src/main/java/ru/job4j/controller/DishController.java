a722b0683eec407b47a62ae6a54d2079b8dbdc48
digest15 <digest15@yandex.ru>
2023-05-14 00:40:08 +0800

    #338433
    Create CRUD and Rest Service

---
+++ src/main/java/ru/job4j/controller/DishController.java
@@ -0,0 +1,76 @@
+package ru.job4j.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.job4j.domain.Dish;
+import ru.job4j.service.DishService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class DishController {
+
+    private final DishService dishService;
+
+    @GetMapping("/")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Dish> finaAll() {
+        return dishService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Dish findById(@PathVariable int id) {
+        Optional<Dish> dish = dishService.findById(id);
+        if (dish.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    String.format("Not found Dish with id %d", id)
+            );
+        }
+        return dish.get();
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Dish create(@RequestBody Dish dish) {
+        Optional<Dish> saved = dishService.save(dish);
+        if (saved.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There was a problem when save Dish");
+        }
+        return saved.get();
+    }
+
+    @PutMapping("/")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Dish update(@RequestBody Dish dish) {
+        Optional<Dish> updated = dishService.save(dish);
+        if (updated.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There was a problem when update Dish");
+        }
+        return updated.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        boolean deleted = dishService.delete(id);
+        if (!deleted) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    String.format("There was a problem when delete Dish by id: %d", id)
+            );
+        }
+        return deleted
+                ? ResponseEntity.ok(true)
+                : ResponseEntity.badRequest()
+                    .body(String.format("There was a problem when delete Dish by id: %d", id));
+    }
+
+}
